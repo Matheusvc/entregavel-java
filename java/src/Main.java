72cdaf9a3e91ba46222d3a5bec2db11afabad869
Matheus Lira <matheusvitorcl1@gmail.com>
2026-08-24 10:40:38 -0300

    entregavel01

---
+++ java/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+
+
+        System.out.println("Algumas informções de quem é Anne:");
+
+
+        int idade = 18;
+        double altura = 1.70;
+        System.out.println("Anne tem " + idade + " Anos, mede " + altura + ", é cristã, quer cursar Medicina e é uma mulher incrível! ");
+
+
+
+    }
+}
